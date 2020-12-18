45a97a5e54e6b0b4a589c46312529647dfa3ba31
Chris Aquino <chris@radishmouse.com>
2020-12-18 16:28:53 -0500

    adds template for movies

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const es6Renderer = require('express-es6-template-engine');
 
+
 const app = express();
 const server = http.createServer(app);
 const logger = morgan('dev');
@@ -13,17 +14,35 @@ app.set('view engine', 'html');
 
 const PORT = 3000;
 const homeRouter = require('./routers/homeRouter');
-
+const data = require('./data.json');
 app.use(logger);
-// app.use((req, res, next) => {
-//     console.log(`${req.method} ${req.url} ${res.statusCode}`);
-//     next();
-// });
-
 app.use('/', homeRouter);
-// app.get('/', (req, res) => {
-//     res.send(`<h1>Hello World!</h1>`);
-// });
+
+// my goal is to render the data (from data.json)
+// to a template
+app.get('/movies', (req, res) => {
+    // res.send(data);
+    console.log(data);
+    // const movieHtmlArray = [];
+    // for (let d of data) {
+    //     movieHtmlArray.push(`<p>${d.title}</p>`);
+    // }
+    // ---
+    // .map() is a loop that creates a new array
+    // and fills it with a "transformed" version
+    // of each element.
+    // const movieHtmlArray = data.map(d => `<p>${d.title}!!</p>`);
+    // const movieHtmlString = movieHtmlArray.join('');
+    // --- previous two lines can be combined:
+    // const movieHtmlString = data.map(d => `<p>!!${d.title}!!</p>`).join('');
+    // console.log(movieHtmlString);
+
+    res.render('movieList', {
+        locals: {
+            movies: data
+        }
+    });
+});
 
 server.listen(PORT, () => {
     console.log(`Listening at port ${PORT}`);
